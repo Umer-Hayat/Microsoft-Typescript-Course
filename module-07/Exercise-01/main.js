f31fca64f48c101248d53707f24a415289877e84
Umer Hayat <umerhayat520@gmail.com>
2024-12-30 23:32:58 +0100

    module07 lab work

---
+++ module-07/Exercise-01/main.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var greetings_module_1 = require("./greetings_module");
+var allGreetingFunctions = require("./greetings_module");
+var greetings_utilities_module_1 = require("./greetings-utilities_module");
+(0, greetings_module_1.returnGreeting)('hello!');
+allGreetingFunctions.returnGreeting('can you see me');
+(0, greetings_utilities_module_1.returnGreeting)('I am here!');
---
+++ module-07/Exercise-01/main.ts
@@ -0,0 +1,8 @@
+import { returnGreeting } from "./greetings_module";
+import * as allGreetingFunctions from "./greetings_module";
+import { returnGreeting as returnGreetingLength } from "./greetings-utilities_module";
+
+
+returnGreeting('hello!')
+allGreetingFunctions.returnGreeting('can you see me');
+returnGreetingLength('I am here!');
